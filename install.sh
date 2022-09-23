d7f0b6f81746acc6f89560f7068b8d7238d8200e
AlphaBril <florian.marie.doucet@gmail.com>
2022-09-23 17:19:56 +0200

    close to the end

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-apt update && apt install -y xfonts-base xserver-xorg-input-all x11-xserver-utils xinit xserver-xorg xserver-xorg-video-all slim bspwm sxhkd rofi dunst pulseaudio pavucontrol thunar blueman psmisc hsetroot polybar python3-pip curl
+apt update && apt install -y xfonts-base xserver-xorg-input-all x11-xserver-utils xinit xserver-xorg xserver-xorg-video-all slim bspwm sxhkd rofi dunst pulseaudio pavucontrol thunar blueman psmisc hsetroot polybar python3-pip curl libcogl-pango-dev libgtkd-3-0 libgdk-pixbuf-2.0-0 ligdk3.0-cil build-essential ligtk-3-dev
 pip3 install wpgtk
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 rustup toolchain install nightly
 git clone https://github.com/elkowar/eww
-apt install -y libcogl-pango-dev libgtkd-3-0 libgdk-pixbuf-2.0-0
 cd eww && cargo build --release
+chmod +x target/release/eww
+mv target/release/eww /usr/bin/eww
+curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
