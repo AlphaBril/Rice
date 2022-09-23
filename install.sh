5e7939a7fba7865c008c53da7af5ef85abe03eb5
AlphaBril <florian.marie.doucet@gmail.com>
2022-09-23 15:21:59 +0200

    asd

--- install.sh
+++ install.sh
@@ -1,8 +1,1 @@
-sudo apt-get update && apt-get install -y \
-xfonts-base xserver-xorg-input-all xinit xserver-xorg xserver-xorg-video-all \ #x11 minimal installation
-slim \ #login manager
-bspwm sxhkd \ #window manager
-rofi \ # App runner
-dunst \ # notifications
-pulseaudio pavucontrol \ #sound
-thunar \ # File manager
+apt update && apt install -y xfonts-base xserver-xorg-input-all xinit xserver-xorg xserver-xorg-video-all slim bspwm sxhkd rofi dunst pulseaudio pavucontrol thunar
